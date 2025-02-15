36f93cd5cfeed48f64161070e3cd0e8e05553361
DenizBayrak <deniz.bayrak7580@gmail.com>
2025-02-15 23:41:23 +0300

    Fixed Problem 27

--- LeetcodeProblems/src/P0027RemoveElement/Solution.java
+++ LeetcodeProblems/src/P0027RemoveElement/Solution.java
@@ -4,14 +4,23 @@ public class Solution {
 
     public static int removeElement(int[] nums, int val) {
         
+        int index = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+        	
+            if (nums[i] != val) {
+            	
+                nums[index] = nums[i]; 
+                index++;
+            }
+        }
     	
-    	
-    	return 0;
+    	return index;
     }
 	
 	public static void main(String[] args) {
 		
-		int[] nums = {2, 2, 3, 4, 7, 2};
+		int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
 		int val = 2; 
 		
 		int k = Solution.removeElement(nums, val);
