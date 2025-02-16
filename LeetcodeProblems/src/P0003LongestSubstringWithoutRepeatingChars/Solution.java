c48cdb1681d40650f79ebaeab6c341c285d8c8f6
DenizBayrak <deniz.bayrak7580@gmail.com>
2025-02-16 19:59:55 +0300

    Added problem 3

---
+++ LeetcodeProblems/src/P0003LongestSubstringWithoutRepeatingChars/Solution.java
@@ -0,0 +1,44 @@
+package P0003LongestSubstringWithoutRepeatingChars;
+
+public class Solution {
+
+	public static int lengthOfLongestSubstring(String s) {
+
+		int maxLength = 0;
+		String longestSubStr = "";
+
+		for (int i = 0; i < s.length(); i++) {
+			
+			StringBuilder subStr = new StringBuilder();
+
+			for (int j = i; j < s.length(); j++) {
+				
+				if (subStr.toString().contains(String.valueOf(s.charAt(j)))) {
+					
+					break;
+				}
+				
+				subStr.append(s.charAt(j));
+			}
+
+			if (subStr.length() > maxLength) {
+				
+				maxLength = subStr.length();
+				longestSubStr = subStr.toString();
+			}
+		}
+
+		System.out.println(longestSubStr);
+		
+		return maxLength;
+	}
+
+	public static void main(String[] args) {
+		
+		String input = "pwwkew";
+		
+		int output = Solution.lengthOfLongestSubstring(input); 
+		
+		System.out.println(output);
+	}
+}
