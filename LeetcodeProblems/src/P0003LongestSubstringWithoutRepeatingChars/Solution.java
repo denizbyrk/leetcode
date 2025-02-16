e038df45c4beb7d0d2c1441119db018b017035f2
DenizBayrak <deniz.bayrak7580@gmail.com>
2025-02-16 20:11:50 +0300

    Revert "Added problem 3"
    This reverts commit 18ceeb4f6bf7908509c363182288d5f07110ecf8.

--- LeetcodeProblems/src/P0003LongestSubstringWithoutRepeatingChars/Solution.java
+++ LeetcodeProblems/src/P0003LongestSubstringWithoutRepeatingChars/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
 	public static int lengthOfLongestSubstring(String s) {
 
 		int maxLength = 0;
-		String longestSubstring = "";
+		String longestSubStr = "";
 
 		for (int i = 0; i < s.length(); i++) {
 			
@@ -24,11 +24,11 @@ public class Solution {
 			if (subStr.length() > maxLength) {
 				
 				maxLength = subStr.length();
-				longestSubstring = subStr.toString();
+				longestSubStr = subStr.toString();
 			}
 		}
 
-		System.out.println(longestSubstring);
+		System.out.println(longestSubStr);
 		
 		return maxLength;
 	}
