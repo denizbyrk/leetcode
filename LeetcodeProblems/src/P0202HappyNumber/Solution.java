315a278f5e2bea2548b98b321bf4f93e7aa82486
DenizBayrak <deniz.bayrak7580@gmail.com>
2025-02-23 23:55:41 +0300

    Added problem 202

---
+++ LeetcodeProblems/src/P0202HappyNumber/Solution.java
@@ -0,0 +1,45 @@
+package P0202HappyNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+	public static boolean isHappy(int n) {
+
+        List<Integer> existingNums = new ArrayList<Integer>();
+
+        while (n != 1 && !existingNums.contains(n)) {
+        	
+        	System.out.println(existingNums);
+        	
+        	existingNums.add(n);
+            
+            String nums = "" + n;
+            char[] ints = nums.toCharArray();
+            int result = 0;
+
+            for (int i = 0; i < ints.length; i++) {
+            	
+                int a = Character.getNumericValue(ints[i]);
+                
+                result += a * a;
+            }
+
+            System.out.println(n);
+            n = result;
+        }
+
+        if (n == 1) return true;
+        return false;
+	}
+
+	public static void main(String[] args) {
+
+		int input = 20;
+
+		boolean output = Solution.isHappy(input);
+
+		System.out.println(output);
+	}
+}
