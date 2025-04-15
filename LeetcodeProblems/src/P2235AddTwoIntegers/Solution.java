42b2ed72bf6bc91922895333efa545d0abed186d
DenizBayrak <deniz.bayrak7580@gmail.com>
2025-04-15 23:54:37 +0300

    Added problem 2235

---
+++ LeetcodeProblems/src/P2235AddTwoIntegers/Solution.java
@@ -0,0 +1,19 @@
+package P2235AddTwoIntegers;
+
+public class Solution {
+
+    public static int sum(int num1, int num2) {
+        
+    	return num1 + num2;
+    }
+    
+    public static void main(String[] args) {
+    	
+    	int num1 = -10;
+    	int num2 = 4;
+    	
+    	int output = Solution.sum(num1, num2);
+    	
+    	System.out.println(output);
+    }
+}
